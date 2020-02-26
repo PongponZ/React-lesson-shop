f9c959fe5f22a50f2190972bb793082bf76bc08d
PongponZ <mr.pongpon.sinlapa.m.m@gmail.com>
2020-02-26 20:03:55 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/hompage.component';
+import {BrowserRouter,Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
+const NoteFoundPage = () =>(
+  <div>
+    NoteFoundPage
+  </div>
+);
+
 
 
 function App() {
   return (
     <div>
-       <Homepage/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/hats' component={HatsPage} />
+          <Route path='/NotFound' component={NoteFoundPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
